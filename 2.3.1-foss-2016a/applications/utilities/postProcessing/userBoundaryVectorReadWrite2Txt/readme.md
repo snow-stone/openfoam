73bde6a8858cdbbfb02adfdd68666f96f23a4412
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-10-31 16:19:13 +0100

    userBoundaryVectorReadWrite2Txt

---
+++ 2.3.1-foss-2016a/applications/utilities/postProcessing/userBoundaryVectorReadWrite2Txt/readme.md
@@ -0,0 +1,21 @@
+# Utility
+userBoundaryVectorReadWrite2Txt
+
+## Version
+2.3.1-foss-2016a
+
+## Prototype
+range
+
+## Functionality
+- 对boundaryField上的某一个patch, 对一个vectorField 的某个分量 求个max min mean
+- 可选择输出为纯txt格式方便python后处理
+
+## How To Use
+userBoundaryVectorReadWrite2Txt wallShearStress -time '5' wall
+
+## Limit
+
+## Cope with limit
+
+## Note
---
+++ 2.3.1-foss-2016a/applications/utilities/postProcessing/userBoundaryVectorReadWrite2Txt/userBoundaryVectorReadWrite2Txt.C
@@ -0,0 +1,129 @@
+#include "fvCFD.H"
+#include "argList.H"
+#include "timeSelector.H"
+
+#include <fstream> // 标准输出，到文件
+//using std::ofstream; // 不加这个class ofstream找不到
+//
+
+void scalarField_simpleStatistics(const scalarField& s)
+{
+    Info<< "Simple Statistics on field : " << endl;
+	Info<< "size of field : " << s.size() << endl;
+	Info<< "min : " << min(s) << endl;
+	Info<< "max : " << max(s) << endl;
+	Info<< "athemtic mean : " << sum(s)/s.size() << endl;
+	Info<< nl;
+}
+
+int main(int argc, char *argv[])
+{
+    Info << "Hello world" << endl;
+
+    timeSelector::addOptions();
+	Foam::argList::addBoolOption
+	(
+	    "noWrite",
+	    "no output data to file"
+    );
+    argList::validArgs.append("fieldName");
+    argList::validArgs.append("patchName");
+
+	#include "setRootCase.H"
+    #include "createTime.H"
+
+	word fieldName(args.additionalArgs()[0]);
+	word patchName(args.additionalArgs()[1]);
+	bool noWriting = args.optionFound("noWrite");
+
+	instantList timeDirs = timeSelector::select0(runTime, args);
+
+	#include "createMesh.H"
+
+	if(!noWriting)
+	{
+	    mkDir("postProcessing");
+	}
+
+	forAll(timeDirs, timeI)
+	{
+		runTime.setTime(timeDirs[timeI], timeI); 
+	    Info<< "Time = " << runTime.timeName() << endl;
+
+        IOobject header
+        (
+            fieldName,
+            runTime.timeName(), 
+            mesh,
+            IOobject::MUST_READ
+        );
+
+	    if (header.headerOk())
+	    {
+		    Info<< "headerOk for field : " << fieldName << endl;
+
+		    //volScalarField s(header, mesh);
+		    //surfaceScalarField s(header, mesh);
+		    volVectorField s(header, mesh);
+		    const polyBoundaryMesh& pp = mesh.boundaryMesh();
+			const label patchLabel = pp.findPatchID(patchName);
+
+			if (patchLabel != -1)
+			{
+				// Downgraded to scalarField. But... boundaryField() has no dimension so
+				// it is not really a downgrade. (Meaning it's different when taking max
+				// of an internalField(). The output will give its dimension etc...
+				vectorField& s_FieldOnPatch = s.boundaryField()[patchLabel];
+				Info<< "On patch " << patchName << endl;
+
+				Info<< "field " << fieldName << " component X "<< endl;
+				scalarField_simpleStatistics(s_FieldOnPatch.component(vector::X));
+
+				const surfaceScalarField& magSf = mesh.magSf();
+				//Info<< magSf.boundaryField()[patchLabel] << endl;
+				Info<< "field magSf " << endl;
+				scalarField_simpleStatistics(magSf.boundaryField()[patchLabel]);
+
+    			Info<< fieldName 
+    				<< " surface weighted average :"
+    				<< sum(s_FieldOnPatch * magSf.boundaryField()[patchLabel])
+					          /
+					       sum(magSf.boundaryField()[patchLabel]) << endl;
+
+			}
+
+			if (!noWriting)
+			{
+
+				if (patchLabel != -1)
+				{
+        	        std::ofstream txtOutput
+        	        (
+        	   	        fileName(
+        				    string("postProcessing")/string("txtBoundary_"+patchName+"_"+fieldName+"_"+runTime.timeName())
+        				).c_str(),
+        		        ios_base::app
+        	        );
+        
+        			forAll(s.boundaryField()[patchLabel], i)
+        			{
+        			    txtOutput
+        				    //<< s.boundaryField()[patchLabel][i] // not implemented here. output a vector
+        				    << s.boundaryField()[patchLabel][i].component(vector::X) // a scalar it is self-evident how to output to a file
+        				    << std::endl;
+        			}
+				}
+				else
+				{
+				    Info<< "No patch named " << patchName << endl;
+				}
+			}
+	    }
+	    else
+	    {
+		    Info<< "No " << fieldName << " input" << endl;
+	    }
+    }
+
+    return 0;
+}
