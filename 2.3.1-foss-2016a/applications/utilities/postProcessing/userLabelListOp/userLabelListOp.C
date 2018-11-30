005db5187a63aed9e7eb980c09f72f592d146d0c
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-11-30 09:10:55 +0100

    read mean field ; check average on slice OK

--- 2.3.1-foss-2016a/applications/utilities/postProcessing/userLabelListOp/userLabelListOp.C
+++ 2.3.1-foss-2016a/applications/utilities/postProcessing/userLabelListOp/userLabelListOp.C
@@ -1,51 +1,108 @@
 #include "fvCFD.H"
 #include "argList.H"
 #include "timeSelector.H"
+#include "cellSet.H"
 
 int main(int argc, char *argv[])
 {
     timeSelector::addOptions();
+    argList::validArgs.append("velocityFieldName");
+	argList::validArgs.append("timeOfAverageField");
+
     #include "setRootCase.H"
     #include "createTime.H"
+
+	word velocityFieldName(args.additionalArgs()[0]);
+	word timeOfAverageField(args.additionalArgs()[1]);
+
     instantList timeDirs = timeSelector::select0(runTime, args);
     #include "createMesh.H"
 
-
-	IOList<label> cellSet
+	IOList<label> slice
 	(
 	    IOobject
 	    (
 		    "selectCells",
-		    runTime.time().constant(),
+		    "constant",
 			mesh,
 		    IOobject::MUST_READ,
 		    IOobject::NO_WRITE	
 		)	
 	);
 
-	volVectorField U
-	(
-	    IOobject
-		(
-	        "U",
-	        runTime.timeName(),
-	        mesh,
-	        IOobject::MUST_READ,
-		    IOobject::NO_WRITE	
-		),
-		mesh
-	);
+    volVectorField UMean
+    (
+        IOobject
+        (
+           velocityFieldName+"_mean",
+		   timeOfAverageField,
+           mesh,
+           IOobject::MUST_READ,
+		   IOobject::NO_WRITE
+        ),
+        mesh
+    );
 
-	/*
-	forAll(cellSet, i)
+    vectorField slice_U(slice.size());
+    vectorField slice_U_mean(slice.size());
+
+    vectorField sum(slice.size(), vector::zero);
+    Info<<" sum = " << sum << endl;
+	label nField = 0;
+
+    forAll(slice, i)
+    {
+        slice_U_mean[i] = UMean.internalField()[slice[i]];
+    	if (i < 3)
+    	{
+    	    Info<< "UMean[" << slice[i] <<"] : "<< slice_U_mean[i] << endl;
+    	}
+   	}
+
+	forAll(timeDirs, timeI)
 	{
-	    Info<< "U[" << cellSet[i] << "] =" << U.internalField()[cellSet[i]] << endl;
+		runTime.setTime(timeDirs[timeI], timeI);
+		Info<< "Time = " << runTime.timeName() << endl;
+
+	    volVectorField U
+	    (
+	        IOobject
+		    (
+	            velocityFieldName,
+	            runTime.timeName(),
+	            mesh,
+	            IOobject::MUST_READ,
+		        IOobject::NO_WRITE	
+		    ),
+		    mesh
+	    );
+
+    	//Info<< "slice : " << slice << endl;
+    	Info<< "size(slice) : " << slice.size() << endl;
+    
+    	forAll(slice, i)
+    	{
+    	    slice_U[i] = U.internalField()[slice[i]];
+    		if (i < 3)
+    		{
+    		    Info<< "U[" << slice[i] <<"] : "<< slice_U[i] << endl;
+    		}
+    	}
+
+		sum += slice_U;
+		nField++;
 	}
-	*/
 
-	for (label i=0; i < 10; i++)
+	if(nField >= 1)
 	{
-	    Info<< "U[" << cellSet[i] << "] =" << U.internalField()[cellSet[i]] << endl;
+	    Info<<"labelList op mean : " << endl;
+    	forAll(slice, i)
+		{
+    		if (i < 3)
+    		{
+    	    	Info<< "U[" << slice[i] <<"] : "<< sum[i]/nField << endl;
+    		}
+    	}
 	}
 
     return 0;
