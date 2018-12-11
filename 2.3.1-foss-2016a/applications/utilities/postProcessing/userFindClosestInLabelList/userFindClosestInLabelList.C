d8c1c2ddee5e0e569d004fd922e8f17487282593
hluo <haining.luo@doctorant.ec-lyon.fr>
2018-12-11 17:01:09 +0100

    read 3 list : vector, word, label. To get the cell whose cell is the closest to ref vector

--- 2.3.1-foss-2016a/applications/utilities/postProcessing/userFindClosestInLabelList/userFindClosestInLabelList.C
+++ 2.3.1-foss-2016a/applications/utilities/postProcessing/userFindClosestInLabelList/userFindClosestInLabelList.C
@@ -1,57 +1,87 @@
 #include "fvCFD.H"
 #include "argList.H"
 #include "timeSelector.H"
-#include "cellSet.H"
-
-#include <fstream>
 
 int main(int argc, char *argv[])
 {
     timeSelector::addOptions();
 	argList::validArgs.append("sliceStore");
-	argList::validArgs.append("sliceName");
+	argList::validArgs.append("slicePrefix");
 
     #include "setRootCase.H"
     #include "createTime.H"
 
 	word sliceStore(args.additionalArgs()[0]);
-	word sliceName(args.additionalArgs()[1]);
+	word slicePrefix(args.additionalArgs()[1]);
 
     instantList timeDirs = timeSelector::select0(runTime, args);
-    #include "createMesh.H"
 
-	IOList<label> slice
+	WarningIn("User defined here : only one reference vector !") << endl;
+	IOList<vector> refVectors
 	(
-	    IOobject
-	    (
-		    sliceName,
+        IOobject
+		(
+			"refVectors",
 			sliceStore,
-			mesh,
-		    IOobject::MUST_READ,
-		    IOobject::NO_WRITE	
-		)	
+			runTime,
+			IOobject::MUST_READ,
+			IOobject::NO_WRITE
+		)
 	);
 
-	label cellN  = slice.size();
-	vector ref(0.0, 0.0, 0.0);
-	scalarField distance(cellN, 0.0);
+	vector ref = refVectors[0];
+	Info << "ref : " << ref << endl;
 
-	WarningIn("only first 3 elements here") << endl;
-	forAll(slice, i)
+    IOList<word> sliceNumber
+	(
+	    IOobject
+		(
+		    "sliceNumber",
+		    sliceStore,
+		    runTime,
+		    IOobject::MUST_READ,
+		    IOobject::NO_WRITE	
+		)
+	);
+	Info << sliceNumber << endl;
+
+    #include "createMesh.H"
+
+	forAll(sliceNumber, i)
 	{
-		distance[i] = Foam::sqrt
-				(
-				 (mesh.C()[slice[i]] - ref) & (mesh.C()[slice[i]] - ref)
-				);
+	
+		IOList<label> slice
+		(
+		    IOobject
+		    (
+			    slicePrefix+sliceNumber[i],
+				sliceStore,
+				mesh,
+			    IOobject::MUST_READ,
+			    IOobject::NO_WRITE	
+			)	
+		);
+	
+		label cellN  = slice.size();
+		scalarField distance(cellN, 0.0);
+	
+		//WarningIn("only first 3 elements here") << endl;
+		forAll(slice, i)
+		{
+			distance[i] = Foam::sqrt
+					(
+					 (mesh.C()[slice[i]] - ref) & (mesh.C()[slice[i]] - ref)
+					);
+		}
+	
+		label l_min = findMin(distance);
+	    Info << "index for min " << slice[l_min] << nl
+			 << "coord : "
+			 << mesh.C()[slice[l_min]] << nl
+			 << "distance : "
+			 << distance[l_min]
+			 << endl;
 	}
-
-	label l_min = findMin(distance);
-    Info << "index for min " << slice[l_min] << "\n"
-		 << "coord : "
-		 << mesh.C()[slice[l_min]] << "\n"
-		 << "distance : "
-		 << distance[l_min]
-		 << endl;
 
     return 0;
 }
