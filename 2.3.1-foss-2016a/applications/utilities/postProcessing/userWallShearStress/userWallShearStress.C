365e74bdaef0617ecf8cf3509c48730e2e643fed
hluo <haining.luo@doctorant.ec-lyon.fr>
2019-02-25 15:28:18 +0100

    for pre-exitsting tabs

--- 2.3.1-foss-2016a/applications/utilities/postProcessing/userWallShearStress/userWallShearStress.C
+++ 2.3.1-foss-2016a/applications/utilities/postProcessing/userWallShearStress/userWallShearStress.C
@@ -45,14 +45,14 @@ Description
 scalar scalarField_simpleStatistics(const scalarField& s)
 {
     Info<< "Simple Statistics on field : " << endl;
-	Info<< "size of field : " << s.size() << endl;
-	Info<< "min : " << min(s) << endl;
-	Info<< "max : " << max(s) << endl;
-	scalar mean = sum(s)/s.size(); 
-	Info<< "athemtic mean : " << mean << endl;
-	Info<< nl;
+    Info<< "size of field : " << s.size() << endl;
+    Info<< "min : " << min(s) << endl;
+    Info<< "max : " << max(s) << endl;
+    scalar mean = sum(s)/s.size(); 
+    Info<< "athemtic mean : " << mean << endl;
+    Info<< nl;
 
-	return mean;
+    return mean;
 }
 
 void calcIncompressible
@@ -89,26 +89,25 @@ int main(int argc, char *argv[])
     timeSelector::addOptions();
     argList::validArgs.append("fieldName");
     argList::validArgs.append("patchName");
-	Foam::argList::addBoolOption
-	(
-		"noWrite",
-		"no output data to file"
-	);
+    Foam::argList::addBoolOption
+    (
+        "noWrite",
+        "no output data to file"
+    );
 
     #include "addRegionOption.H"
 
-    #include "setRootCase.H"
-    #include "createTime.H"
+    #include "setRootCase.H" #include "createTime.H"
     instantList timeDirs = timeSelector::select0(runTime, args);
-	word fieldName(args.additionalArgs()[0]);
-	word patchName(args.additionalArgs()[1]);
-	bool noWriting = args.optionFound("noWrite");
+    word fieldName(args.additionalArgs()[0]);
+    word patchName(args.additionalArgs()[1]);
+    bool noWriting = args.optionFound("noWrite");
     #include "createNamedMesh.H"
 
-	if(!noWriting)
-	{
-		mkDir("postProcessing");
-	}
+    if(!noWriting)
+    {
+        mkDir("postProcessing");
+    }
 
     forAll(timeDirs, timeI)
     {
@@ -151,70 +150,70 @@ int main(int argc, char *argv[])
             volVectorField U(UHeader, mesh);
             calcIncompressible(mesh, runTime, U, wallShearStress);
 
-			const polyBoundaryMesh& pp = mesh.boundaryMesh();
-			const label patchLabel = pp.findPatchID(patchName);
-			volScalarField::GeometricBoundaryField d = nearWallDist(mesh).y(); //#include "nearWallDist.H"
+            const polyBoundaryMesh& pp = mesh.boundaryMesh();
+            const label patchLabel = pp.findPatchID(patchName);
+            volScalarField::GeometricBoundaryField d = nearWallDist(mesh).y(); //#include "nearWallDist.H"
 
-			if (patchLabel != -1)
-			{
-				vectorField& tauByRho = wallShearStress.boundaryField()[patchLabel];
-				Info<< "On patch " << patchName << endl;
-				//
-				Info<< "field " << wallShearStress.name() << " component(vector::X)" << endl;
-				scalar mean0 = scalarField_simpleStatistics(tauByRho.component(vector::X));
-				//
-				const surfaceScalarField& magSf = mesh.magSf();
-				scalar mean1 = sum(tauByRho.component(vector::X) * magSf.boundaryField()[patchLabel])
-								/
-							   sum(magSf.boundaryField()[patchLabel]);
-				Info<< "surface weighted average : " << mean1 << endl;	   
-				//
-				Info<< "field " << wallShearStress.name() << " mag" << endl;
-				scalar mean2 = scalarField_simpleStatistics(mag(tauByRho));
-				//
-				scalar uTau = Foam::sqrt(mean2);
-				Info<< "uTau " << uTau << endl;
-				//
-				Info<< "field " << "d" << "[" << patchLabel << "]" << " on patch " << patchName << endl;
-				scalar dMean = scalarField_simpleStatistics(d[patchLabel]);
-				//
-				scalarField yPlus = uTau * d[patchLabel] / 1.0e-6;
-				Info<< "!!!!!!!!!!!!!!!!"<< endl;
-				Info<< "nu : " << 1.0e-6 << endl;
-				Info<< "!!!!!!!!!!!!!!!!"<< endl;
-				Info<< "field " << "yPlus" << endl;
-				scalar yPlusMean = scalarField_simpleStatistics(yPlus);
-				//
-				scalarField yPlus1 = mag(tauByRho) * d[patchLabel] / 1.0e-6;
-				Info<< "field " << "yPlus1" << endl;
-				scalar yPlusMean1 = scalarField_simpleStatistics(yPlus1);
+            if (patchLabel != -1)
+            {
+                vectorField& tauByRho = wallShearStress.boundaryField()[patchLabel];
+                Info<< "On patch " << patchName << endl;
+                //
+                Info<< "field " << wallShearStress.name() << " component(vector::X)" << endl;
+                scalar mean0 = scalarField_simpleStatistics(tauByRho.component(vector::X));
+                //
+                const surfaceScalarField& magSf = mesh.magSf();
+                scalar mean1 = sum(tauByRho.component(vector::X) * magSf.boundaryField()[patchLabel])
+                                /
+                               sum(magSf.boundaryField()[patchLabel]);
+                Info<< "surface weighted average : " << mean1 << endl;       
+                //
+                Info<< "field " << wallShearStress.name() << " mag" << endl;
+                scalar mean2 = scalarField_simpleStatistics(mag(tauByRho));
+                //
+                scalar uTau = Foam::sqrt(mean2);
+                Info<< "uTau " << uTau << endl;
+                //
+                Info<< "field " << "d" << "[" << patchLabel << "]" << " on patch " << patchName << endl;
+                scalar dMean = scalarField_simpleStatistics(d[patchLabel]);
+                //
+                scalarField yPlus = uTau * d[patchLabel] / 1.0e-6;
+                Info<< "!!!!!!!!!!!!!!!!"<< endl;
+                Info<< "nu : " << 1.0e-6 << endl;
+                Info<< "!!!!!!!!!!!!!!!!"<< endl;
+                Info<< "field " << "yPlus" << endl;
+                scalar yPlusMean = scalarField_simpleStatistics(yPlus);
+                //
+                scalarField yPlus1 = mag(tauByRho) * d[patchLabel] / 1.0e-6;
+                Info<< "field " << "yPlus1" << endl;
+                scalar yPlusMean1 = scalarField_simpleStatistics(yPlus1);
 
-				if (!noWriting)
-				{
-					std::ofstream txtOutput
-					(
-						fileName(
-								string("postProcessing")/string("txtWallShearStressCmptXMean_Boundary_"+patchName+"_"+fieldName)
-								).c_str(),
-						ios_base::app
-					);
+                if (!noWriting)
+                {
+                    std::ofstream txtOutput
+                    (
+                        fileName(
+                                string("postProcessing")/string("txtWallShearStressCmptXMean_Boundary_"+patchName+"_"+fieldName)
+                                ).c_str(),
+                        ios_base::app
+                    );
 
-					forAll(wallShearStress.boundaryField()[patchLabel], i)
-					{
-						txtOutput
-							<< runTime.timeName() << " "
-							<< mean0 << " "
-							<< mean1 << " "
-							<< std::endl;
-					}
-				}
-			}
-			else
-			{
-				Info<< "no patch named " << patchName << endl;
-				Info<< "taking break ..." << endl;
-				break;
-			}
+                    forAll(wallShearStress.boundaryField()[patchLabel], i)
+                    {
+                        txtOutput
+                            << runTime.timeName() << " "
+                            << mean0 << " "
+                            << mean1 << " "
+                            << std::endl;
+                    }
+                }
+            }
+            else
+            {
+                Info<< "no patch named " << patchName << endl;
+                Info<< "taking break ..." << endl;
+                break;
+            }
 
         }
         else
